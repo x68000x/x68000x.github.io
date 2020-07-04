b173a3f7bf468b728e0cc85b5197f1484ee87a9c
Magic Chicken Team <68000@gmx.net>
2020-07-04 21:35:47 +0200

    Add files via upload

---
+++ data/level/textures.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="textures" tilewidth="16" tileheight="16" tilecount="1024" columns="32">
+ <image source="../image/textures.png" width="520" height="520"/>
+</tileset>
